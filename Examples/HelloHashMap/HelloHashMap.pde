0a6fb8352c162a6aa82ae79f6b5d92aec9fa06e4
Alessandro Pasqualini <alessandro.pasqualini.1105@gmail.com>
2016-04-30 16:46:30 +0200

    Divisi header e implementazione, workaround problema template

--- Examples/HelloHashMap/HelloHashMap.pde
+++ Examples/HelloHashMap/HelloHashMap.pde
@@ -1,25 +1,20 @@
 #include <HashMap.h>
 
-//define the max size of the hashtable
-const byte HASH_SIZE = 5; 
-//storage 
-HashType<char*,int> hashRawArray[HASH_SIZE]; 
-//handles the storage [search,retrieve,insert]
-HashMap<char*,int> hashMap = HashMap<char*,int>( hashRawArray , HASH_SIZE ); 
+//define HashMap
+HashMap<String, int> hashMap;
 
 void setup(){
   //setup hashmap
-  hashMap[0]("name",18);
-  hashMap[1]("test",200);
-  hashMap[2]("qwer",1234);
-  hashMap[3]("abc",123);
-  hashMap[4]("AlphaBeta",20);
+  hashMap.put("name",18);
+  hashMap.put("test",200);
+  hashMap.put("qwer",1234);
+  hashMap.put("abc",123);
+  hashMap.put("AlphaBeta",20);
   
   Serial.begin(9600);
-  Serial.println( hashMap.getIndexOf("test"),DEC );
-  Serial.println( hashMap.getValueOf("test") );
+
+  Serial.println(hashMap.valueFor("test"), DEC );
   
-  hashMap.debug();
 }
 
 void loop(){/*nothing to loop*/}
--- HashMap.h
+++ HashMap.h
@@ -14,278 +14,154 @@
 || | Wiring Cross-platform Library
 || #
 ||
-|| @license Please see cores/Common/License.txt.
+|| @license Please see LICENSE.
 ||
+*/
+
+
+/*
+	WARNING!!!!!!!!!
+
+	Header file: 	HashMap.h
+	Cpp File:		HashMap_Impl.h
+
+	La suddivisione è stata così imposta per aggirare il problema dei template e allo stesso
+	tempo mantenere la suddivisione tra Header e Implementazione.
+
 */
 
 #ifndef HASHMAP_H
 #define HASHMAP_H
 
-template<typename hash,typename map>
-class HashType {
-public:
-  HashType() { reset(); }
-  HashType(hash code, map value): hashCode(code), mappedValue(value) { }
 
-  void reset() {
-    hashCode = 0; mappedValue = 0;
-  }
+//---HASH TYPE---
 
-  hash getHash() const {
-    return hashCode;
-  }
-
-  void setHash(hash code){
-    hashCode = code;
-  }
-
-  map getValue() const {
-    return mappedValue;
-  }
-
-  void setValue(map value){
-    mappedValue = value;
-  }
-
-  HashType& operator()(hash code, map value){
-    setHash(code);
-    setValue(value);
-  }
-private:
-  hash hashCode;
-  map mappedValue;
-};
-
-
+//class template
 template<typename hash, typename map>
+
+//class HashType
+class HashType {
+
+	public:
+
+		//--costruttori
+		HashType();
+		HashType(hash code, map value);
+		~HashType(); //distruttore
+
+		void reset();
+		
+		hash getHash() const;
+
+		void setHash(hash code);
+
+		map getValue() const;
+
+		void setValue(map value);
+
+		//HashType& operator()(hash code, map value);
+
+
+	private:
+		
+		hash hashCode;
+		map mappedValue;
+};
+
+
+
+//---HASH NODE---
+
+//class template
+template<typename hash, typename map>
+
+//class HashNode
 class HashNode {
-public:
-  HashNode(hash code, map value) {
-    hashType = new HashType<hash, map>(code, value);
-    previus = 0;
-    next = 0;
-  }
 
-  ~HashNode() {
-    delete hashType;
-  }
+	public:
 
-  HashType<hash, map> * getHashType() const {
-    return hashType;
-  }
+		HashNode(hash code, map value);
 
-  HashNode<hash, map> * getPrevius() const {
-    return previus;
-  }
+		~HashNode();
 
-  HashNode<hash, map> * getNext() const {
-    return next;
-  }
+		HashType<hash, map> *getHashType() const;
 
-  void setPrevius(HashNode<hash, map> * previus) {
-    this->previus = previus;
-  }
+		HashNode<hash, map> *getPrevius() const;
 
-  void setNext(HashNode<hash, map> * next) {
-    this->next = next;
-  }
+		HashNode<hash, map> *getNext() const;
 
-private:
-  HashType<hash, map> * hashType;
-  HashNode * previus;
-  HashNode * next;
+		void setPrevius(HashNode<hash, map> *previus);
+
+		void setNext(HashNode<hash, map> *next);
+
+
+	private:
+
+		HashType<hash, map> *hashType;
+		HashNode *previus;
+		HashNode *next;
 };
 
 
-template<typename hash,typename map>
+
+//---HASH MAP---
+//Classe principale
+
+//class template
+template<typename hash, typename map>
+
+//class HashMap
 class HashMap {
-public:
-  HashMap(){  
-    start = 0;
-    finish = 0;
-    position = 0; 
-    size = 0;
-  }
 
-  ~HashMap(){
-    if (moveToFirst()) {
-      HashNode<hash, map> * next = position;
-      while (position) {
-        next = position->getNext();
-        delete position;
-        position = next;
-      }
-    }
-  }
+	public:
 
-  void put(hash key, map value) { 
-    if (start == 0) {
-      start = finish = new HashNode<hash, map>(key, value);
-    } else {
-      if (hashPairForKey(key)!=0) {
-        remove(hashPairForKey(key));
-      }
-      HashNode<hash, map> * temp = new HashNode<hash, map>(key, value);
-      finish->setNext(temp);
-      temp->setPrevius(finish);
-      finish = temp;
-    }
-    size++;
-  }
+		HashMap();
+		~HashMap();
 
-  bool containsKey(hash key) const {
-    return hashPairForKey(key) != 0;
-  }
+		void put(hash key, map value);
 
-  map valueFor(hash key) {
-    HashNode<hash, map> * pointer = hashPairForKey(key);
-    if (pointer != 0) {
-      return pointer->getHashType()->getValue();
-    }
-  }
+		bool containsKey(hash key) const;
 
-  map valueAt(int i) {
-    moveToFirst();
-    int iter = 0;
-    do {
-      if (iter == i) {
-        return position->getHashType()->getValue();
-      }
-      iter++;
-    } while (moveToNext());
-  }
+		map valueFor(hash key);
 
-  map value() {
-    if (position != 0) {
-      return position->getHashType()->getValue();
-    }
-  }
+		map valueAt(int i);
 
-  hash keyFor(map value) {
-    moveToFirst();
-    do {
-      if (position->getHashType()->getValue() == value) {
-        return position->getHashType()->getHash();
-      }
-    } while (moveToNext());
-  }
+		map value();
 
-  hash keyAt(int i) {
-    moveToFirst();
-    int iter = 0;
-    do {
-      if (iter == i) {
-        return position->getHashType()->getHash();
-      }
-      iter++;
-    } while (moveToNext());
-  }
+		hash keyFor(map value);
 
-  hash key() {
-    if (position != 0) {
-      return position->getHashType()->getHash();
-    }
-  }
+		hash keyAt(int i);
 
-  void remove(hash key) {
-    HashNode<hash, map> * pointer = hashPairForKey(key);
-    if (pointer != 0) {
-      remove(pointer);
-    }
-  }
+		hash key();
 
-  void remove() {
-    if (position != 0) {
-      if (size == 1) {          
-        remove(position);
-        position = 0;
-      } else {
-        if (position == start) {
-          remove(position);
-          position = start;
-        } else {
-          remove(position);
-          position = position->getPrevius();
-        }
-      }
-    }
-  }
+		void remove(hash key);
 
-  unsigned int count() const {return size; }
+		void remove();
 
-  bool moveToFirst() {
-    if (start != 0) {
-      position = start;
-      return true;
-    } else {
-      return false;
-    }
-  }
+		unsigned int count() const;
 
-  bool moveToLast() {
-    if (finish != 0) {
-      position = finish;
-      return true;
-    } else {
-      return false;
-    }
-  }
+		bool moveToFirst();
 
-  bool moveToNext() {
-    if (position->getNext() != 0) {
-      position = position->getNext();
-      return true;
-    } else {
-      return false;
-    }
-  }
+		bool moveToLast();
 
-  bool moveToPrev() {
-    if (position->getPrevius() != 0) {
-      position = position->getPrevius();
-      return true;
-    } else {
-      return false;
-    }
-  }
+		bool moveToNext();
 
-private:
+		bool moveToPrev();
 
-  HashNode<hash, map> * hashPairForKey(hash key) {
-    for(HashNode<hash, map> * pointer = start; pointer != 0; pointer = pointer->getNext()) {
-      HashType<hash, map> * hashType = pointer->getHashType();
-      if (key == hashType->getHash()) {
-        return pointer;
-      }
-    }
-    return 0;
-  }
 
-  void remove(HashNode<hash, map> * pointer) {
-    if (pointer == 0) return;
-    if (size == 1) {
-      start = finish = 0;
-    } else {
-      if (pointer == start) {
-        start = start->getNext();
-        start->setPrevius(0);
-      } else if (pointer == finish) {
-        finish = finish->getPrevius();
-        finish->setNext(0);
-      } else {
-        pointer->getPrevius()->setNext(pointer->getNext());
-        pointer->getNext()->setPrevius(pointer->getPrevius());
-      }
-    }
+	private:
 
-    size--;
-    delete pointer;
-  }
+		HashNode<hash, map> *hashPairForKey(hash key);
 
-  HashNode<hash, map> * start;
-  HashNode<hash, map> * finish;
-  HashNode<hash, map> * position;
-  int size;
+		void remove(HashNode<hash, map> *pointer);
+
+		HashNode<hash, map> *start;
+		HashNode<hash, map> *finish;
+		HashNode<hash, map> *position;
+		int size;
 };
-#endif
-// HASHMAP_H
+
+//incluso per workaround problema template
+#include "HashMap_Impl.h"
+
+#endif // HASHMAP_H
